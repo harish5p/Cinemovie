9130e7cbd7a6f6e3654dbdcf0aab7749af9695eb
Harish Pentapalli <harish5p@users.noreply.github.com>
2023-03-01 11:09:12 +0000

    useFetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,31 +1,16 @@
-import { useState, useEffect } from "react";
+import { useFetch } from "../hooks/useFetch";
 import { Card } from "../components";
 
-
-
-export const MovieList = () => {
-  const [movies,setMovies] = useState([]);
-
-  useEffect(() =>  {
-    async function fetchMovies(){
-      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=26786d6daf9f6b1dbed57bc6e2db1892");
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export const MovieList = ({apiPath}) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap">
-          {movies.map((movie) => (
+        <div className="flex justify-start flex-wrap">       
+          { movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
-          ))}
-          
-
-
-
+          )) }          
         </div>
       </section>
     </main>
