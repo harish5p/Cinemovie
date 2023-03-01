b5c872824fc202a69e708805cbbc737f413f1a2d
Harish Pentapalli <harish5p@users.noreply.github.com>
2023-03-01 11:20:57 +0000

    tailwind css customization

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,10 +24,10 @@ export const Header = () => {
   return (
     <header>
 
-      <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900 dark:border-b-1">
+      <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2  dark:bg-gray-900 dark:border-b-1">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="h-6 mr-3 sm:h-9" alt="CineMovie Logo" />
+            <img src={Logo} className="h-6 mr-2 sm:h-9" alt="CineMovie Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CineMovie</span>
           </Link>
 
